e482ba2d6293c16f182978257278aac6f1e77121
youxiho1 <750440234@qq.com>
2017-11-25 21:03:39 +0800

    3.1

--- app/src/main/java/com/example/youxihouzainali/shopping/MarginActivity.java
+++ app/src/main/java/com/example/youxihouzainali/shopping/MarginActivity.java
@@ -1,13 +1,28 @@
 package com.example.youxihouzainali.shopping;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MarginActivity extends AppCompatActivity {
 
+    String uname = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_margin);
+        Intent intent = getIntent();
+        uname = intent.getStringExtra("extra_data");
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MarginActivity.this, NewMarginActivity.class);
+                intent.putExtra("extra_data", uname);
+                startActivity(intent);
+            }
+        });
     }
 }
